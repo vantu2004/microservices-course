c8a3f7a95b29e70c235a782cdfec47f8544cda52
vantu <tulevan526@gmail.com>
2026-01-19 11:30:14 +0700

    refactor(dto): switch contact info DTOs from record to class and enable actuator endpoints

--- accounts/src/main/java/com/easybytes/accounts/dto/AccountContactInfoDto.java
+++ accounts/src/main/java/com/easybytes/accounts/dto/AccountContactInfoDto.java
@@ -1,11 +1,17 @@
 package com.easybytes.accounts.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "accounts")
-public record AccountContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
-
+@Getter
+@Setter
+public class AccountContactInfoDto {
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 }
